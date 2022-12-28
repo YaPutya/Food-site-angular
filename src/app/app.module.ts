a9ab09127414295c2225b52ed97a4c6341485439
YaPutya <nikita12-0101@mail.ru>
2022-12-28 20:55:06 +0300

    search + routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { RatingModule } from 'ng-starrating';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FoodPageComponent } from './components/pages/food-page/food-page.component';
 import { HomeComponent } from './components/pages/home/home.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, SearchComponent, FoodPageComponent],
   imports: [BrowserModule, AppRoutingModule, RatingModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/services/food.service.ts
+++ src/app/services/food.service.ts
@@ -3,13 +3,23 @@ import { sample_foods } from 'src/data';
 import { Food } from '../shared/models/Food';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FoodService {
-
-  constructor() { }
+  constructor() {}
 
   getAll(): Food[] {
     return sample_foods;
   }
+
+  getAllFoodsBySearchTerm(searchTerm: string) {
+    return this.getAll().filter((food) =>
+      food.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  }
+
+  getFoodById(foodId: string): Food {
+    return this.getAll().find(food => food.id  == foodId) ?? new Food()
+  }
+
 }
